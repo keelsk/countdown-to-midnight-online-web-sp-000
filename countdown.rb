462715c1604118c33c59b14d85185f68e720c18e
Kirsten Keels <keelsk@gmail.com>
2020-02-23 03:34:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 #write your code here
-
+require 'pry'
 def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
   end 
   puts "HAPPY NEW YEAR!"
+  binding.pry
 end
